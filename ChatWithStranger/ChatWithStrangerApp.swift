846b9417210eeabc703429deee53cee9ef740302
long-brivvio <69776642+long-brivvio@users.noreply.github.com>
2022-05-05 21:52:11 +1000

    Initial Commit

---
+++ ChatWithStranger/ChatWithStrangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithStrangerApp.swift
+//  ChatWithStranger
+//
+//  Created by Long Nguyen on 5/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithStrangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
